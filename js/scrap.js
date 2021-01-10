aaf88db79228492d0d46b8ef25b29e8175ec6959
carolinsanna <59510922+carolinsanna@users.noreply.github.com>
2021-01-09 21:20:08 -0300

    Scrapbook criado

---
+++ js/scrap.js
@@ -0,0 +1,123 @@
+var scrapList = [];
+var obj;
+var objTitle;
+
+function adicionaRecado(){
+    var message = obj.value;
+    var title = objTitle.value;
+
+    /*var index = scrapList.findIndex(scrap => scrap.item.toLowerCase() === message.toLowerCase());
+
+    if(index >= 0){
+        alert("Já existe");
+        return;
+    }*/
+
+    if(message === ""){
+        alert("Insira uma mensagem!");
+        return;
+    }
+    
+    if(title === ""){
+        alert("Insira um título!");
+        return;
+    }
+
+    message = message.replaceAll("'", "");
+        
+    scrapList.push({
+        item: message,
+        tit: title
+    });
+
+    obj.value = "";
+    objTitle.value = "";
+    obj.focus();
+
+    mostraRecado(scrapList);
+    saveData(scrapList);
+
+    var divAlert = document.getElementById("alerta");
+    divAlert.style.display = "block";
+
+    setTimeout(function() {
+        divAlert.style.display = "none";
+    }, 3000);
+
+}
+
+function saveData(list){
+    localStorage.setItem("id", JSON.stringify(list));
+}
+
+window.addEventListener('load', function(){
+    loadData();
+    obj = document.getElementById('recado');
+    objTitle = document.getElementById('title');
+
+    var botao = document.getElementById('enviar');
+    botao.addEventListener('click', function(){
+        adicionaRecado();
+    });
+    
+    var divAlert = document.getElementById("alerta");
+    divAlert.style.display = "none";
+
+    obj.addEventListener('keypress', function(ev){
+        if(ev.key === "Enter"){
+            ev.preventDefault();
+            adicionaRecado();
+        }
+    });
+});
+
+
+
+function loadData(){
+    var saveList = localStorage.getItem('id');
+    if(saveList !== null){
+        scrapList = JSON.parse(saveList);
+    } else {
+        scrapList = [];
+    }
+    mostraRecado(scrapList);
+}
+
+function mostraRecado(list){
+    var scrap = document.getElementById('scraps');
+    
+    var novoHTML = "";
+
+    for (var scr of list){
+        novoHTML += `
+                <div class="col-md-4">
+                    <div class="card mb-3 text-center">
+                        <div class="card-body">
+                            <h5 class="card-title">${scr.tit}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted"></h6>
+                            <p class="card-text">${scr.item}</p>
+                            <button type="button" class="btn btn-danger" onclick = "apagar('${scr.item}')">Apagar</button>
+                        </div>
+                    </div>
+                </div>
+        `;
+    }
+
+    scrap.innerHTML = novoHTML;
+}
+
+function apagar(conteudo){
+    var index = scrapList.findIndex(f => f.item === conteudo);
+    if(index < 0){
+        return;
+    }
+
+    if(!confirm(`Deseja excluir?`)){
+        return;
+    }
+
+    scrapList.splice(index, 1);
+
+    mostraRecado(scrapList);
+    saveData(scrapList);
+}
